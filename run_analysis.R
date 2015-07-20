1de96bbb839fe0a80d770546f7f160f28a380bef
tmstevebigdata <tmsteve@gmail.com>
2015-07-20 09:01:45 +0800

    Updated R file

--- run_analysis.R
+++ run_analysis.R
@@ -11,39 +11,26 @@ getwd()
 
 ## Step 3 - Define functions used by the script
 
-## Read a data file into a data frame
-fReadDataIntoDF <- function(readfrom, headerflag)
+## Function - Read a data file into a data frame
+fReadDataIntoDF <- function(readfrom, headerflag = FALSE)
 {
  return(read.table(readfrom, header = headerflag))
 }
 
-## Write data from data frame to text file
-fWriteData2TextFile <- function(writefrom, writeto, rowflag)
+## Function - Write data from data frame to text file
+fWriteData2TextFile <- function(writefrom, writeto, rownamesflag = FALSE)
 {
- return(write.table(writefrom, file = writeto, row.names = rowflag))
+if (file.exists(writeto))
+   {
+    file.remove(writeto)
+   }
+
+ return(write.table(writefrom, file = writeto, row.names = rownamesflag))
 }
 
-## Show data frame info. for debugging purposes
-fShowDFInfo <- function(title, dfname, nooflines = 30L)
-{
- print(paste("Title =", title), quote = FALSE)
- print(paste("No. of lines to show =", nooflines), quote = FALSE)
- print("-- Head --", quote = FALSE)
- print(head(dfname, n = nooflines))
- print("-- Tail --", quote = FALSE)
- print(tail(dfname, n = nooflines))
- print("-- Class --", quote = FALSE)
- print(class(dfname))
- print("-- Structure --", quote = FALSE)
- print(str(dfname))
+## Step 4 - Include libraries used by the script
 
- return(1)
-}
-
-## Step 4 - Include libraries used by this script
-
-library(data.table)
-library(dplyr)
+library(data.table, quietly = TRUE)
 
 ## Step 5 - Read 'features' and 'activity labels' data to data frames
 
@@ -85,55 +72,51 @@ df_combined_features <- rbind(df_train_features, df_test_features)
 
 colnames(df_combined_subject) <- "Subject"
 colnames(df_combined_activity) <- "Activity"
-colnames(df_combined_features) <- t(df_features[2])
+colnames(df_combined_features) <- t(df_features[2]) # Perform a transform on features names to change rows to columns
 
-## Step 10 - Merges the training and the test sets to create one data set
+## Step 10 - Merges the training and the test data sets to create one data set
 
 df_AllCombinedData <- cbind(df_combined_features, df_combined_activity, df_combined_subject)
 
-## Step 11 - Extracts only the measurements on the mean and standard deviation for each measurement
+## Step 11 - Extract only the measurements on the mean and standard deviation for each measurement
 
-sstr <- ".*Mean.*|.*Std.*"
-MeanSTDDataOnly <- grep(sstr, names(df_AllCombinedData), ignore.case=TRUE)
-Data2Extract <- c(MeanSTDDataOnly, 562, 563)
-fShowDFInfo("Data2Extract", Data2Extract, 20L)
-## dim(df_AllCombinedData)
+MeanSTDDataOnly <- grep("*mean*|*std*", names(df_AllCombinedData), ignore.case=TRUE)
+ActivityOnly <- grep("Activity", names(df_AllCombinedData), ignore.case=FALSE)
+SubjectOnly <- grep("Subject", names(df_AllCombinedData), ignore.case=FALSE)
+Data2Extract <- c(MeanSTDDataOnly, ActivityOnly, SubjectOnly)
 df_Extracted_Data <- df_AllCombinedData[, Data2Extract]
-## dim(df_Extracted_Data)
 
-## Step 12 - Uses descriptive activity names to name the activities in the data set
+## Step 12 - Use descriptive activity names to name the activities in the data set
 
 df_Extracted_Data$Activity <- as.character(df_Extracted_Data$Activity)
-for (i in 1:6)
+for (i in 1:length(df_Extracted_Data$Activity))
     {
      df_Extracted_Data$Activity[df_Extracted_Data$Activity == i] <- as.character(df_activity_labels[i, 2])
     }
 df_Extracted_Data$Activity <- as.factor(df_Extracted_Data$Activity)
 
-## Step 13 - Appropriately labels the data set with descriptive variable names
+## Step 13 - Appropriately label the data set with descriptive variable names
 
-names(df_Extracted_Data) <- gsub("Acc", "Accelerometer", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("Gyro", "Gyroscope", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("BodyBody", "Body", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("Mag", "Magnitude", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("^t", "Time", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("^f", "Frequency", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("tBody", "TimeBody", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("-mean()", "Mean", names(df_Extracted_Data), ignore.case = TRUE)
-names(df_Extracted_Data) <- gsub("-std()", "STD", names(df_Extracted_Data), ignore.case = TRUE)
-names(df_Extracted_Data) <- gsub("-freq()", "Frequency", names(df_Extracted_Data), ignore.case = TRUE)
-names(df_Extracted_Data) <- gsub("angle", "Angle", names(df_Extracted_Data))
-names(df_Extracted_Data) <- gsub("gravity", "Gravity", names(df_Extracted_Data))
+df_Ori_Names <- c("Acc", "Gyro", "BodyBody", "Mag", "^t", "^f", "tBody", "-mean()", "-std()", "-freq()", "angle", "gravity")
+df_New_Names <- c("Accelerometer", "Gyroscope", "Body", "Magnitude", "Time", "Frequency", "TimeBody", "Mean", "StandardDeviation", "Frequency", "Angle", "Gravity")
 
-## Step 14 - From the data set in step 13, creates a second, independent tidy data set with the average of
-##           each variable for each activity and each subject
+for (i in 1:length(df_Ori_Names))
+    {
+     names(df_Extracted_Data) <- gsub(df_Ori_Names[i], df_New_Names[i], names(df_Extracted_Data), ignore.case = TRUE)
+    }
+
+## Step 14 - From the data set in step 13, create a second, independent tidy data set with the average of each variable for each activity and each subject
 
 df_Extracted_Data$Subject <- as.factor(df_Extracted_Data$Subject)
 df_Extracted_Data <- data.table(df_Extracted_Data)
 
-td_Tidy_Data <- aggregate(. ~Subject + Activity, df_Extracted_Data, mean)
-td_Tidy_Data <- td_Tidy_Data[order(td_Tidy_Data$Subject,td_Tidy_Data$Activity), ]
+dt_Tidy_Data <- aggregate(. ~Subject + Activity, df_Extracted_Data, mean)
+dt_Tidy_Data <- dt_Tidy_Data[order(dt_Tidy_Data$Subject, dt_Tidy_Data$Activity), ]
 
 ## Step 15 - Finally output the tidy data set to a text file
 
-fWriteData2TextFile(td_Tidy_Data, "tidy_data.txt", FALSE)
+fWriteData2TextFile(dt_Tidy_Data, "tidy_data.txt", FALSE)
+
+## Clear workspace before ending the program
+rm(list=ls())
+ls()
